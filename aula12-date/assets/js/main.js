ef1f49b716f3765c28a7dc8eaf5624894e880c25
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-06-13 16:41:13 -0500

    aula12

---
+++ aula12-date/assets/js/main.js
@@ -0,0 +1,6 @@
+const h1=document.querySelector('.container h1');
+const data=new Date();
+const opcoes = {
+    dateStyle: 'full', timeStyle: 'short'
+};
+h1.innerHTML=data.toLocaleString('pt-BR',opcoes);
