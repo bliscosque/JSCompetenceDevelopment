c143096a843a33f5798afae1b1701b27157fa46b
Thiago <bliscosque@yahoo.com.br>
2022-07-12 16:44:12 -0500

    node

--- node/aula1/app.js
+++ node/aula1/app.js
@@ -7,3 +7,9 @@ falaNome(); //chamo diretamente
 
 const {nome, sobrenome} = require('./mod1'); //desestruturando a importacao
 console.log('Usando de outro modulo: ' + nome+ ' '+ sobrenome )
+
+console.log(__filename); //caminho absoluto para esse arquivo
+console.log(__dirname); //caminho absoluto do path
+
+const path=require('path');
+console.log(path.resolve(__dirname,'..','..','aula1'))
