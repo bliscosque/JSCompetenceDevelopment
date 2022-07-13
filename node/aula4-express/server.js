a8554f4f6ee97233e851b0d04c32aff823955066
Thiago <bliscosque@yahoo.com.br>
2022-07-13 16:59:27 -0500

    continue express

--- node/aula4-express/server.js
+++ node/aula4-express/server.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const app=express();
 const routes=require('./routes');
+const path=require('path')
 
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.resolve(__dirname, 'public'))); //o conteúdo dessa página é referenciado diretamente a partir do /
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
 app.use(routes);
 
 app.listen(8080, () => {
--- node/aula4-express/src/controllers/homeController.js
+++ node/aula4-express/src/controllers/homeController.js
@@ -1,10 +1,11 @@
 exports.paginaInicial = (req,res) => {
-    res.send(`
+/*     res.send(`
     <form action="/" method="POST">
     Nome Cliente: <input type="text" name="nome">
     <button>Postar</button>
     </form>
-    `);
+    `); */
+    res.render('index'); //nao precisa da extensao -> .ejs
 }
 
 exports.trataPost = (req, res) => {
