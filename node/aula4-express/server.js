3e294f192169f8cc422307ebb41c465c8e8cf605
Thiago <bliscosque@yahoo.com.br>
2022-07-13 13:08:58 -0500

    express - cont

--- node/aula4-express/server.js
+++ node/aula4-express/server.js
@@ -1,9 +1,28 @@
 var express=require('express') 
 var app=express()
 
+app.use(express.urlencoded({extended:true}));
+
+
 app.get('/', function(req,res) {
-    res.send("Hello World!");
+    res.send(`
+    <form action="/" method="POST">
+    Nome Cliente: <input type="text" name="nome">
+    <button>Postar</button>
+    </form>
+    `);
 })
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('recebi o form');
+})
+
 app.listen(8080, () => {
     console.log('Servidor executando na porta 8080 -> http://localhost:8080');
+});
+
+app.get('/profiles/:idUser?', (req, res) => {
+    console.log(req.query);
+    res.send('ola mundo');
 });
