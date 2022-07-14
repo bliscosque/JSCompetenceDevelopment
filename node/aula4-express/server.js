23c5dd691656560ed8a4a53d2a14f87d485fb83d
Thiago <bliscosque@yahoo.com.br>
2022-07-14 11:03:54 -0500

    flash and session

--- node/aula4-express/server.js
+++ node/aula4-express/server.js
@@ -6,16 +6,43 @@ const mongoose=require('mongoose')
 const connectionString=process.env.CONNECTIONSTRING; //do .env
 mongoose.connect(connectionString).then(() => app.emit('done')).catch(e => console.log(e)); //emitir um sinal quando a conexao estiver ok
 
+
+const session=require('express-session');
+const MongoStore=require('connect-mongo');
+const flash=require('connect-flash')
+
 const routes=require('./routes');
 const path=require('path')
-const meuMiddleware=require('./src/middlewares/middleware')
+const meuMiddleware=require('./src/middlewares/middleware');
+const { Store } = require('express-session');
+
+
+
+
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.resolve(__dirname, 'public'))); //o conteúdo dessa página é referenciado diretamente a partir do /
+
+app.use(flash());
+const sessionOptions=session({
+    secret: 'ahusadsb c ahasoidjsaidj',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000*60*60*24*7, //7 dias em ms
+        httpOnly: true
+    }
+});
+app.use(sessionOptions);
+
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 app.use(meuMiddleware); //todas as requisicoes precisam passar pelo meuMiddleware
 app.use(routes);
+
+
 
 //apenas quando emitir o sinal de done, comece o servidor
 app.on('done', () => {
--- node/aula4-express/src/controllers/homeController.js
+++ node/aula4-express/src/controllers/homeController.js
@@ -1,10 +1,10 @@
 const HomeModel= require('../models/HomeModel')
 
-HomeModel.create({
+/* HomeModel.create({
     titulo: 'titulo de teste',
     descricao: 'descricao tambem de testes'
 }).then( dados => console.log(dados))
-.catch(e => console.log(e));
+.catch(e => console.log(e)); */
 
 /* {
     titulo: 'titulo de teste',
@@ -14,10 +14,21 @@ HomeModel.create({
 } */
 
 //buscando todos os dados
-HomeModel.find().then( dados => console.log(dados))
-.catch(e => console.log(e));
+/* HomeModel.find().then( dados => console.log(dados))
+.catch(e => console.log(e)); */
 
 exports.paginaInicial = (req,res) => {
+    //req.session.usuario = {nome: 'Thiago', logado: true};
+    console.log(req.session.usuario) // o objeto fica por 7 dias, mesmo se nao tiver o parametro acima
+
+    //define flash message
+    //req.flash('info', 'Olá Mundo!'); 
+    //req.flash('error', 'Um erro!');
+    //req.flash('success', 'Sucesso!');
+
+    //busca a flash message, depois de mostrar uma vez ao usuario, apaga da BD
+    console.log(req.flash('info'));
+
     res.render('index'); //nao precisa da extensao -> .ejs
 }
 
