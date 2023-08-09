72f2633ece7d8dc6ef1e79f7f605a21da6f47a85
Thiago <bliscosque@yahoo.com.br>
2023-08-08 22:33:02 -0600

    started electron chat app

---
+++ electron/book_getting_started/C1-chat-app/index.js
@@ -0,0 +1,11 @@
+const { app,BrowserWindow } = require("electron");
+
+function createWindow() {
+    let win=new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+    win.loadFile("index.html")
+    win.webContents.openDevTools()
+}
+app.whenReady().then(createWindow)
