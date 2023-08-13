335a425a967f39bdd2c69fbe4dbc4c7e84897784
Thiago <bliscosque@yahoo.com.br>
2023-08-12 23:44:51 -0600

    DOM - intro

---
+++ HTML/withJS/01-DOM/index.js
@@ -0,0 +1,10 @@
+document.querySelector("#third").innerHTML="New text";
+document.querySelector("#third").textContent="New text2";
+let ggl=document.getElementsByClassName("first")[0];
+ggl.style.color="red";
+let btn=document.getElementsByTagName("button")[0];
+btn.style.backgroundColor="yellow";
+
+document.querySelector("h1").classList.add("h1big")
+
+document.querySelector("a").setAttribute("href","http://www.bing.com");
