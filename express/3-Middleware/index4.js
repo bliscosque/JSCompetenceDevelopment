a8b9c5ea1f04394f81728f43d28d11b70273bf9e
Thiago <bliscosque@yahoo.com.br>
2023-08-21 10:57:06 -0600

    more on middleware

--- express/3-Middleware/index4.js
+++ express/3-Middleware/index4.js
@@ -1,7 +1,22 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit",(req,res) => {
+  console.log(req.body)
+  let bName=req.body.street+req.body.pet;
+  res.send(`Band name: ${bName}`)
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
