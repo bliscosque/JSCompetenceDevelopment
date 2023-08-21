5e0a5356119ab40ac6e83ed7be510e21da2d5605
Thiago <bliscosque@yahoo.com.br>
2023-08-21 09:06:57 -0600

    Thiago APP - clearing table

--- electron/ThiagoAPP/links.js
+++ electron/ThiagoAPP/links.js
@@ -3,11 +3,21 @@ const YAML=require('yaml')
 
 document.querySelector("#btnLinks").addEventListener("click",() => {
     console.log("links clicked");
-    const file=fs.readFileSync('./links.yaml', 'utf-8')
+    const tLinks=document.querySelector("#tLinks")
+
+    //remove all existing links b4 populating it
+    const tableRows = tLinks.getElementsByTagName('tr');
+    let rowCount = tableRows.length;
+
+    for (let x=rowCount-1; x>0; x--) {
+        tLinks.removeChild(tableRows[x]);
+     }
+
+
+    const file=fs.readFileSync('./links.yaml', 'utf-8');
     
     const links=YAML.parse(file);
-    
-    const tLinks=document.querySelector("#tLinks")
+
     console.log(links.links.length)
 
     for (let i=0;i<links.links.length;i++) {
