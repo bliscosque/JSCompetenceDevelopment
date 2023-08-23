87dd5f03fca1f4a70d56f531b193033935155e98
Thiago <bliscosque@yahoo.com.br>
2023-08-23 16:44:18 -0600

    EJS passing data

---
+++ express/4-EJS/3- Passing Data/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render('index.ejs')
+});
+
+app.post("/submit", (req, res) => {
+  let fName=req.body.fName
+  let lName=req.body.lName
+  let cnt=fName.length+lName.length
+  console.log(fName,lName)
+  res.render('index.ejs',{
+    cnt:cnt
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
