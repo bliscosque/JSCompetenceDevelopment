52ac98a3ba4c680ad74e7508347d0d38dd8d418c
Thiago <bliscosque@yahoo.com.br>
2023-09-02 15:39:30 -0600

    todolist - continue - design and landing page

---
+++ 102-project_todoList/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import bodyParser from "body-parser"
+const app=express()
+const port=3000
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.listen(port,() => {
+    console.log(`Server running on port ${port}`)
+})
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
