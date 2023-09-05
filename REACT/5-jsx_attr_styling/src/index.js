8be0be605820e51bb37c6010e15696ed70b26761
Thiago <bliscosque@yahoo.com.br>
2023-09-05 15:58:39 -0600

    styling

---
+++ REACT/5-jsx_attr_styling/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 className="cblue">My Favourite Foods</h1>
+    <img className="picture" src="https://picsum.photos/id/237/200/300" alt="dog"></img>
+    <img className="picture" src="https://picsum.photos/id/14/200/200" alt="beach"></img>
+    <img className="picture" src="https://picsum.photos/id/144/200/200" alt="other" />
+  </div>,
+  document.getElementById("root")
+);
