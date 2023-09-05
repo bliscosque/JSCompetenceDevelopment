126b90d56ea241c74651c3e4dba8dbb2d9186b6b
Thiago <bliscosque@yahoo.com.br>
2023-09-05 16:45:00 -0600

    components

---
+++ REACT/7-components/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div>
+        <Heading />
+        <ul>
+          <li>Bacon</li>
+          <li>Jamon</li>
+          <li>Noodles</li>
+        </ul>
+      </div>     
+    )
+}
+
+export default App
