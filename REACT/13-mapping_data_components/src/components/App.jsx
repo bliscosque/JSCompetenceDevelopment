f4cd2708f7039521b21d185cd688b7e785aec15d
Thiago <bliscosque@yahoo.com.br>
2023-09-09 22:28:28 -0600

    react map

---
+++ REACT/13-mapping_data_components/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function createCard(contact) {
+  return <Card key={contact.id} id={contact.id} name={contact.name} imgURL={contact.imgURL} phone={contact.phone} email={contact.email}/>
+}
+
+function App() {
+  return (
+    <div>
+        <h1 className="heading">My Contacts</h1>
+        {contacts.map(createCard)}
+    </div>
+    
+  );
+}
+
+export default App;
