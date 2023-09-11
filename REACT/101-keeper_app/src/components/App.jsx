9b0a8109192f1728b114e4fc9a2ebd606562e434
Thiago <bliscosque@yahoo.com.br>
2023-09-11 11:06:33 -0600

    keeper - P2

--- REACT/101-keeper_app/src/components/App.jsx
+++ REACT/101-keeper_app/src/components/App.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function genNote(note) {
+    return (<Note key={note.key} title={note.title} content={note.content} />)
+}
+
 
 function App() {
-    return <div><Header /><Note /> <Footer /></div>
+    return <div><Header />{notes.map(genNote)} <Footer /></div>
 }
 
 export default App
