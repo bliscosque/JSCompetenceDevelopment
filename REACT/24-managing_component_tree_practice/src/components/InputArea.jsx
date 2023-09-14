70d99ab857feb64fef4a77d7d376a4c2d004baef
Thiago <bliscosque@yahoo.com.br>
2023-09-14 12:38:17 -0600

    managing components practice

---
+++ REACT/24-managing_component_tree_practice/src/components/InputArea.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function InputArea(props) {
+  const [inputText, setInputText] = React.useState("");
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+    
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={props.inputText} />
+      <button onClick={() => {
+        props.onAdd(inputText)
+        setInputText("")
+        }}>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
