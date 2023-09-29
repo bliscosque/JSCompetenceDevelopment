2fba50c1fb9ca438cad1d8a29c8415e4da9dd1e4
Thiago <bliscosque@yahoo.com.br>
2023-09-28 22:17:17 -0600

    rn - components

---
+++ React_Native_Udemy/02-components_overview/src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Text,StyleSheet} from "react-native";
+
+const ComponentsScreen=() => {
+  return <Text>This is a txt</Text>
+}
+
+const styles=StyleSheet.create({
+  textStyle: {
+    fontSize: 30
+  }
+})
+
+export default ComponentsScreen;
