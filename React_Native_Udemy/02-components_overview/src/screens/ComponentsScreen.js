75371a89e45d295ce03cd15f9fa86ed1a834523b
Thiago <bliscosque@yahoo.com.br>
2023-09-29 19:47:34 -0600

    exercise16

--- React_Native_Udemy/02-components_overview/src/screens/ComponentsScreen.js
+++ React_Native_Udemy/02-components_overview/src/screens/ComponentsScreen.js
@@ -1,13 +1,20 @@
 import React from "react";
-import {Text,StyleSheet} from "react-native";
+import {View,Text,StyleSheet} from "react-native";
 
 const ComponentsScreen=() => {
-  return <Text style={styles.textStyle}>This is a txt</Text>
+  let name="Thiago"
+  return <View>
+    <Text style={styles.textStyle}>This is a txt</Text>
+    <Text style={styles.subTextStyle}>App by: {name}</Text>
+  </View>
 }
 
 const styles=StyleSheet.create({
   textStyle: {
     fontSize: 40
+  },
+  subTextStyle: {
+    fontSize: 20
   }
 })
 
