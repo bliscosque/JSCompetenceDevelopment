942a392314f5ba461f47ab8076df42473833d73a
Thiago <bliscosque@yahoo.com.br>
2023-11-28 06:45:23 -0600

    RN Layout

--- React_Native_Udemy/07-layout_system/src/screens/BoxScreen.js
+++ React_Native_Udemy/07-layout_system/src/screens/BoxScreen.js
@@ -4,12 +4,23 @@ import {Text,StyleSheet,View,TextInput} from "react-native";
 const BoxScreen = () => {
     const [txt,setTxt]=useState("");
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>Box Screen</Text>
         </View>
     )
 }
 
-const styles=StyleSheet.create({  })
+const styles=StyleSheet.create({ 
+    viewStyle: {
+        borderWidth: 1,
+        borderColor: `black`,
+    },
+    textStyle: {
+        borderWidth: 1,
+        borderColor: 'red',
+        marginVertical: 20,
+        marginHorizontal: 20,
+    }
+ })
 
 export default BoxScreen;
