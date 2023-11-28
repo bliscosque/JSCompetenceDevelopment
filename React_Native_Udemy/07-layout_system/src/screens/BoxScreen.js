dba499bf8fa2143097a788b40680c4c3c3988f64
Thiago <bliscosque@yahoo.com.br>
2023-11-28 08:29:30 -0600

    RN - continue layouts

--- React_Native_Udemy/07-layout_system/src/screens/BoxScreen.js
+++ React_Native_Udemy/07-layout_system/src/screens/BoxScreen.js
@@ -4,8 +4,18 @@ import {Text,StyleSheet,View,TextInput} from "react-native";
 const BoxScreen = () => {
     const [txt,setTxt]=useState("");
     return (
+        <View>
+        
         <View style={styles.viewStyle}>
             <Text style={styles.textStyle}>Box Screen</Text>
+        </View>
+
+        <View style={styles.view2Style}>
+            <Text>Child 1</Text>
+            <Text>Child 2</Text>
+            <Text>Child 3</Text>
+        </View>
+
         </View>
     )
 }
@@ -20,7 +30,12 @@ const styles=StyleSheet.create({
         borderColor: 'red',
         marginVertical: 20,
         marginHorizontal: 20,
-    }
+    },
+    view2Style: {
+        borderWidth: 3,
+        flexDirection: 'row',
+        alignItems: 'flex-end'
+    },
  })
 
 export default BoxScreen;
