88135cf0467c6cd1a62d73efc6ad92f5028e8b6a
Thiago <bliscosque@yahoo.com.br>
2023-12-20 14:15:05 -0600

    RN - more on food app

---
+++ React_Native_Udemy/food/src/components/searchBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {View,Text,StyleSheet, TextInput} from 'react-native';
+import { Feather } from '@expo/vector-icons'; 
+
+const SearchBar = ({term, onTermChange}) => {
+    return (<View style={styles.backgroundStyle}>
+        <Feather name="search" style={styles.iconStyle} color="black" />
+        <TextInput autoCapitalize="none" autoCorrect="false" 
+            style={styles.inputStyle} placeholder="Search" value={term} onChangeText={newTerm=>onTermChange(newTerm)}/>
+    </View>)
+};
+
+const styles=StyleSheet.create({
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row',
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18,
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15,
+    }
+});
+
+export default SearchBar;
--- React_Native_Udemy/food/src/screens/searchScreen.js
+++ React_Native_Udemy/food/src/screens/searchScreen.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import {View,Text,StyleSheet} from 'react-native';
-import SearchBar from "./searchBar";
+import SearchBar from "../components/searchBar";
 
 const SearchScreen = () => {
+    const [term,setTerm] = useState('');
     return (<View>
-        <SearchBar />
+        <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)}/>
+        <Text>{term}</Text>
     </View>)
 };
 
