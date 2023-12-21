a1e23aa9e62275a852587ba032b63ecaab1ed5f2
Thiago <bliscosque@yahoo.com.br>
2023-12-21 13:42:05 -0600

    RN - some fault handling

--- React_Native_Udemy/food/src/api/yelp.js
+++ React_Native_Udemy/food/src/api/yelp.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import 'dotenv/config'
+//import 'dotenv/config'
 
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization: 
-            'Bearer ' + process.env.API_KEY,
+            'Bearer ' + process.env.EXPO_PUBLIC_API_KEY,
     }
 });
--- React_Native_Udemy/food/src/screens/searchScreen.js
+++ React_Native_Udemy/food/src/screens/searchScreen.js
@@ -6,21 +6,28 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
     const [term,setTerm] = useState('');
     const [results,setResults]=useState([]);
+    const [errorMessage, setErrorMessage]=useState('');
 
     const searchApi=async () => {
-        const response=yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: 'san jose',
-            }
-        });
-        setResults(response.data.businesses);
+        try {
+            const response=await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'san jose',
+                }
+            });
+            //console.log(response.data.businesses);
+            setResults(response.data.businesses);
+        }catch (err) {
+            setErrorMessage('Something went wrong with the request');
+        }
     }
 
     return (<View>
-        <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={() => console.log("term was submitted")}/>
-        <Text>We have found {results} results</Text>
+        <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={() => searchApi()}/>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
+        <Text>We have found {results.length} results</Text>
     </View>)
 };
 
