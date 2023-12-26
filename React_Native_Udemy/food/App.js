45d38ae6a7840cb42cb3165384e3cf6c3da5f0a6
Thiago <bliscosque@yahoo.com.br>
2023-12-26 16:06:39 -0600

    RN - results on food app

--- React_Native_Udemy/food/App.js
+++ React_Native_Udemy/food/App.js
@@ -1,6 +1,6 @@
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
-import SearchScreen from './src/screens/searchScreen';
+import SearchScreen from './src/screens/SearchScreen';
 
 const navigator=createStackNavigator({
   Search: SearchScreen,
--- React_Native_Udemy/food/src/components/resultsList.js
+++ React_Native_Udemy/food/src/components/resultsList.js
@@ -1,1 +1,25 @@
-import {View, Text, StyleSheet} from 'react';
+import React from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+const ResultsList = ({title, results}) => {
+    return (
+    <View>
+        <Text style={styles.titleStyle}>{title}</Text>
+        <FlatList 
+            horizontal={true} data={results} keyExtractor={(result) =>result.id}
+            renderItem={({item}) => {
+                return <Text>{item.name}</Text>
+            }}
+        />
+    </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    titleStyle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    }
+});
+
+export default ResultsList;
