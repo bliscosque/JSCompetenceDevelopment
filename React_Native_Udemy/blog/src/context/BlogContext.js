a45145be33a3f6e686036e61b4d206a4881c9afc
Thiago <bliscosque@yahoo.com.br>
2024-01-05 14:11:49 -0600

    RN seting up context

---
+++ React_Native_Udemy/blog/src/context/BlogContext.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BlogContext = React.createContext();
+
+const blogPosts=[
+    {title: 'Post #1'},
+    {title: 'Post #2'},
+];
+
+export const BlogProvider=({children}) => {
+    return (<BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>)
+}
+
+export default BlogContext;
