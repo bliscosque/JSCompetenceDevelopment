d642b1b552dccfab5369f861af0a9e9eccdd8e72
Thiago <bliscosque@yahoo.com.br>
2024-01-09 16:59:18 -0600

    adjusting more on index screen

--- React_Native_Udemy/blog/src/screens/IndexScreen.js
+++ React_Native_Udemy/blog/src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React,{useContext} from "react";
 import {View, Text, StyleSheet, FlatList, Button} from "react-native";
 import {Context,Provider} from "../context/BlogContext";
+import {Feather} from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const {state,addBlogPost}=useContext(Context);
@@ -9,11 +10,32 @@ const IndexScreen = () => {
         <Text>Index Screen</Text>
         <Button title="Add Post" onPress={addBlogPost} />
         <FlatList data={state} keyExtractor={(blogPost) => blogPost.title} 
-        renderItem={({item}) => {return (<Text>{item.title}</Text>)}} />
+        renderItem={({item}) => {return (
+          <View style={styles.row}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Feather style={styles.icon} name="trash" />
+            </View>
+          )
+        }} />
     </View>
   );
 };
 
-const styles=StyleSheet.create({});
+const styles=StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
