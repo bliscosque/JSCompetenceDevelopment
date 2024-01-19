c3b443c4a4a4661145d5cec59a866de1cb671634
Thiago <bliscosque@yahoo.com.br>
2024-01-19 16:09:39 -0600

    RN - blog app  - ok

--- React_Native_Udemy/blog/src/context/BlogContext.js
+++ React_Native_Udemy/blog/src/context/BlogContext.js
@@ -6,6 +6,11 @@ const blogReducer = (state,action) => {
             return [...state,{title: action.payload.title, content: action.payload.content, id: Math.floor(Math.random()*99999)}]
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id!==action.payload);
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                if (blogPost.id===action.payload.id) return action.payload;
+                else return blogPost
+            })
         default:
             return state;
     }
@@ -25,8 +30,9 @@ const deleteBlogPost = (dispatch) => {
 }
 
 const editBlogPost = (dispatch) => {
-    return (id, title, content) => {
-        dispatch({type: 'edit_blogpost', payload: {id, title, content}})
+    return (id, title, content, callback) => {
+        dispatch({type: 'edit_blogpost', payload: {id, title, content}});
+        callback();
     }
 }
 
