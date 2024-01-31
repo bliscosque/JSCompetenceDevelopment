92dcf7dbe3f2357b5f252d7c3723143ab4e781bc
Thiago <bliscosque@yahoo.com.br>
2024-01-31 11:05:25 -0600

    track server ok

--- React_Native_Udemy/track-server/src/index.js
+++ React_Native_Udemy/track-server/src/index.js
@@ -1,10 +1,18 @@
 const express=require('express');
 const mongoose=require('mongoose');
-const app=express();
+const bodyParser = require('body-parser');
+require('./models/User')
+require('./models/Track')
 require('dotenv').config()
 const authRoutes=require('./routes/authRoutes');
+const trackRoutes=require('./routes/trackRoutes')
+const requireAuth=require('./middlewares/requireAuth');
 
+
+const app=express();
+app.use(bodyParser.json())
 app.use(authRoutes);
+app.use(trackRoutes)
 
 const mongoURI=`mongodb+srv://bliscosque:${process.env.DB_PASS}@track-server.v4qj2h4.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(mongoURI, {
@@ -20,8 +28,8 @@ mongoose.connection.on('error', (err) => {
     console.log('MongoDB has error to connect: ' + err);
 })
 
-app.get('/', (req,res) => {
-    res.send('Hi there');
+app.get('/', requireAuth, (req,res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
--- React_Native_Udemy/track-server/src/routes/authRoutes.js
+++ React_Native_Udemy/track-server/src/routes/authRoutes.js
@@ -1,8 +1,41 @@
 const express=require('express');
-const router=express.Router();
+const mongoose=require('mongoose');
+const jwt=require('jsonwebtoken')
 
-router.post('/signup', (req,res) => {
-    res.send("You've made a post");
+const router=express.Router();
+const User=mongoose.model('User');
+
+router.post('/signup', async (req,res) => {
+    const {email,password}=req.body;
+    try {
+        const user=new User({email,password})
+        await user.save();
+        const token=jwt.sign({userId: user._id},'MY_KEY')
+        res.send({token});
+    }catch(err) {
+        console.log(err);
+        return res.status(422).send(err.message);
+    }
+}) 
+
+router.post('/signin', async (req,res) => {
+    const {email,password}=req.body;
+    if (!email || !password) {
+        return res.status(422).send({error: 'Must provide email and password'});
+    }
+    const user=await User.findOne({email});
+    if (!user) {
+        return res.status(404).send({error: 'Email not found'})
+    }
+
+    try {
+        await user.comparePassword(password);
+        const token=jwt.sign({userId:user._id},'MY_KEY');
+        res.send({token});
+
+    }catch (err) {
+        return res.status(402).send({error: 'Invalid password or email'})
+    }
 })
 
 module.exports=router;
