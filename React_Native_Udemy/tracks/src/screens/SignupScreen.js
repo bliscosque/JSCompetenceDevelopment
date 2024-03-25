34c3e5051f975ff3dba21bea26c68a44281de764
Thiago <bliscosque@yahoo.com.br>
2024-03-25 10:28:53 -0600

    tracker app - added reactnativeelements and created spacer

--- React_Native_Udemy/tracks/src/screens/SignupScreen.js
+++ React_Native_Udemy/tracks/src/screens/SignupScreen.js
@@ -1,14 +1,43 @@
-import React from "react";
-import {View,StyleSheet,Text,Button} from "react-native";
+import React, {useState} from "react";
+import {View,StyleSheet} from "react-native";
+import { Text,Input, Button } from '@rneui/themed';
+import Spacer from "./components/Spacer";
 
 const SignupScreen = ({navigation}) => {
-    return <>
-    <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-    <Button title="Go to SignIn" onPress={() => navigation.navigate('Signin')} />
-    <Button title="Go to main flow" onPress={() => navigation.navigate('mainFlow')} />
-    </>
+
+    //const 
+
+    return <View style={styles.container}>
+        <Spacer>
+            <Text h3>Signup for Tracker</Text>
+        </Spacer>
+        
+        <Spacer>
+            <Input label="Email" />
+        </Spacer>
+
+        <Spacer>
+            <Input label="Password" />
+        </Spacer>
+        
+        <Spacer>
+            <Button title="Sign Up" />
+        </Spacer>
+    </View>
 };
 
-const styles=StyleSheet.create({ });
+SignupScreen.navigationOptions = () => {
+    return {
+      headerShown: false,
+    };
+  };
+
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 230
+    }
+ });
 
 export default SignupScreen;
